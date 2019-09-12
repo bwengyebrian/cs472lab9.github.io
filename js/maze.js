9e5b6ecb6ae61b7016459ce8f5a5ebe1bb668afc
Brian Bwengye <bwengyebrian@gmail.com>
2019-09-12 01:30:26 -0500

    added js

--- js/maze.js
+++ js/maze.js
@@ -1,12 +1,26 @@
 $(function () {
     var start = false;
     var passed;
+	var boundary = $('.boundary');
+	var status= $('#status');
+	var statusText = status.text();
     $('#start').click(function () {
         start = true;
         passed = true;
+		boundary.removeClass('youlose');
+		status.text(statusText).css('color' , 'black');
     });
-
-    $('.boundary').mouseover(function (e) {
+	$('body').mouseover(function(){
+		if(start){
+			passed = false;
+			boundary.addClass('youlose');
+			}
+		});
+	$('#maze').mouseover(function(){
+			return false;
+		});	
+		
+    boundary.mouseover(function (e) {
         if (start){
             passed = false;
             $(this).addClass('youlose');
@@ -15,9 +29,9 @@ $(function () {
     })
     $('#end').click(function () {
       if(passed){
-          alert('You passed')
+          status.text('Bingoo!! You Win!').css('color','green');
       }else{
-          alert("You failed")
+          status.text('Sorry!! You Lose').css('color','red');
       }
       start = false;
       passed = undefined;
